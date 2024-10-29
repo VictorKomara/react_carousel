32a61b47007c4d741f801e2c5e06f5a343c44d33
Victor Komara <vitioklss@gmail.com>
2024-10-29 15:24:57 +0200

    add task second solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Carousel.scss';
-import cn from 'classnames';
 
 interface Image {
   id: number;
@@ -12,62 +11,48 @@ interface Props {
 }
 
 const Carousel: React.FC<Props> = ({ images }) => {
-  const [curentImages, setCurentImages] = useState<Image[]>(images);
-  const [itemWidth, setItemWidth] = useState(130);
-  const [frameSize, setFrameSize] = useState(3);
-  const [step, setStep] = useState(3);
-  const [curentIndex, setCurentIndex] = useState(0);
-  const startIndex: number = 1;
-  const endIndex: number = 10;
+  const defaultAnimationDuration = 1000;
+  const defaultItemWidth = 130;
+  const defaultFrameSize = 3;
+  const defaultStep = 3;
+  const defaultInfinite = false;
+  const defaultCurentIndex = 0;
+  const [itemWidth, setItemWidth] = useState<number>(defaultItemWidth);
+  const [frameSize, setFrameSize] = useState<number>(defaultFrameSize);
+  const [animationDuration, setAnimationDuration] = useState<number>(
+    defaultAnimationDuration,
+  );
+  const [infinite, setInfinite] = useState<boolean>(defaultInfinite);
+  const [step, setStep] = useState<number>(defaultStep);
+  const [currentIndex, setCurrentIndex] = useState<number>(defaultCurentIndex);
+  const maxIndex = images.length - frameSize;
 
   //console.log(curentIndex);
 
   function handelPrevClick() {
-    const tempImage = [...curentImages];
-    let tempIndex = curentIndex;
-    let tempStep = step;
+    setCurrentIndex(prevIndex => {
+      if (infinite) {
+        return (prevIndex - step + images.length) % images.length;
+      }
 
-    // console.log('curentIndex:', tempIndex);
-    // console.log('step:', tempStep);
-
-    if (curentIndex - step < startIndex) {
-      tempStep = curentIndex - startIndex;
-      tempIndex = curentIndex - tempStep;
-    }
-
-    for (let i: number = 1; i <= tempStep; i++) {
-      tempImage.unshift(tempImage[tempImage.length - 1]);
-      tempImage.pop();
-    }
-
-    setCurentImages(tempImage);
-    setCurentIndex(tempIndex);
+      return Math.max(prevIndex - step, 0);
+    });
   }
 
   function handelNextClick() {
-    const tempImage = [...curentImages];
-    let tempIndex = curentIndex;
-    let tempStep = step;
+    setCurrentIndex(prevIndex => {
+      if (infinite) {
+        return (prevIndex + step) % images.length;
+      }
 
-    // console.log('curentIndex:', tempIndex);
-    // console.log('step:', tempStep);
-
-    if (curentIndex + step < endIndex) {
-      tempStep = endIndex - curentIndex;
-      tempIndex = curentIndex + tempStep;
-    }
-
-    for (let i: number = 1; i <= tempStep; i++) {
-      tempImage.push(tempImage[0]);
-      tempImage.shift();
-    }
-
-    setCurentImages(tempImage);
-    setCurentIndex(tempIndex);
+      return Math.min(prevIndex + step, maxIndex);
+    });
   }
 
+  const translateX = currentIndex > maxIndex ? maxIndex : currentIndex;
+
   return (
-    <div className="Carousel">
+    <>
       <div className="Control-panel">
         <div className="Control-item">
           <label htmlFor="itemWidth">Image width</label>
@@ -105,51 +90,79 @@ const Carousel: React.FC<Props> = ({ images }) => {
             onChange={ev => setStep(Number(ev.target.value))}
           />
         </div>
+
+        <div className="Control-item">
+          <label htmlFor="animationDuration">Animation duration</label>
+          <input
+            type="number"
+            name="animationDuration"
+            id="animationDuration"
+            min="100"
+            max="1000"
+            value={`${animationDuration}`}
+            onChange={ev => setAnimationDuration(Number(ev.target.value))}
+          />
+        </div>
+
+        <div className="Control-item">
+          <label htmlFor="infinite">Infinite</label>
+          <input
+            type="checkbox"
+            name="infinite"
+            id="infinite"
+            checked={infinite}
+            onChange={ev => setInfinite(ev.target.checked)}
+          />
+        </div>
       </div>
 
-      <ul
-        className="Carousel__list"
-        style={{ width: `${frameSize * itemWidth}px` }}
-      >
-        {curentImages.map(image => (
-          <li key={image.id}>
-            <img src={image.src} alt="1" style={{ width: `${itemWidth}px` }} />
-          </li>
-        ))}
-      </ul>
+      <div className="Carousel">
+        <div
+          className="carousel__container"
+          style={{
+            transform: `translateX(-${translateX * itemWidth}px)`,
+            transition: `transform ${animationDuration}ms ease-in-out`,
+            width: `${itemWidth * frameSize}px`,
+          }}
+        >
+          <ul className="Carousel__list">
+            {images.map(image => (
+              <li key={image.id}>
+                <img
+                  src={image.src}
+                  alt={`Image №${image.id}`}
+                  width={itemWidth}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <button
-        type="button"
-        className={cn({
-          disabled: curentIndex === startIndex,
-        })}
-        onClick={() => {
-          handelPrevClick();
-        }}
-      >
-        Prev
-      </button>
+        <div className="carousel__buttons">
+          <button
+            type="button"
+            disabled={!infinite && currentIndex === 0}
+            onClick={() => {
+              handelPrevClick();
+            }}
+          >
+            Prev
+          </button>
 
-      <button
-        type="button"
-        data-cy="next"
-        className={cn({
-          disabled: curentIndex === endIndex,
-        })}
-        onClick={() => {
-          handelNextClick();
-        }}
-      >
-        Next
-      </button>
-    </div>
+          <button
+            type="button"
+            data-cy="next"
+            disabled={!infinite && currentIndex + frameSize >= images.length}
+            onClick={() => {
+              handelNextClick();
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default Carousel;
-
-{
-  /* <div style={{ width: `${myWidth}px` }}>
-    Цей елемент матиме ширину, яка залежить від myWidth
-  </div> */
-}
